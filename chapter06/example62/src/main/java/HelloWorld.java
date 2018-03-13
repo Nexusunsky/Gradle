64eee42b5da9bd1d415b47567b199191ceeb468a
Hao Liu <haoliu@thoughtworks.com>
2018-03-13 23:20:18 +0800

    dependcies on third lib

---
+++ chapter06/example62/src/main/java/HelloWorld.java
@@ -0,0 +1,7 @@
+import okhttp3.OkHttpClient;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("OkHttpClient.Build.class" + OkHttpClient.Builder.class);
+    }
+}
