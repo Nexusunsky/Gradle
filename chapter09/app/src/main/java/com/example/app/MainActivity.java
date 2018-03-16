147463e5fec5663f69a2c3df2290c073e36df753
Hao Liu <haoliu@thoughtworks.com>
2018-03-16 20:47:09 +0800

    useLibrary not from android stand library

--- chapter09/app/src/main/java/com/example/app/MainActivity.java
+++ chapter09/app/src/main/java/com/example/app/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] libraryNames = getPackageManager().getSystemSharedLibraryNames();
+        for (String libraryName : libraryNames) {
+            Log.d(TAG, "libraryNames:" + libraryName);
+        }
     }
 }
